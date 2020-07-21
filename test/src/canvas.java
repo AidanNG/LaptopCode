3e9a2275227a11d55737ae9616bda2b425c43757
AidanNG <54791508+AidanNG@users.noreply.github.com>
2020-07-20 22:29:01 -0500

    Add files via upload

---
+++ test/src/canvas.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+import static java.lang.System.out;
+
+public class canvas {
+    public static void main(String[] args) {
+
+    try{
+        while(true);
+
+    }catch(RuntimeException e){
+        System.out.println("bad");
+    }finally{
+        System.out.println("cool dude");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
